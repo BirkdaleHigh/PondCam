f8fa6390bd4a7d449067dfa9c4591ee01047194f
Mr J. Bennett <jbennett@BHS.INTERNAL>
2016-03-24 11:11:26 +0000

    load the correct websocket url

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-    client = new WebSocket('ws://localhost:9999')
+    client = new WebSocket('ws://' + document.location.hostname + ':9999')
     player = new jsmpeg(client, {
         canvas: document.getElementById('video'),
         autoplay: true
